5746dd4daee4fc87e566150362216af9c43c3dde
Leandra Klein <lok2ea@virginia.edu>
2019-05-29 13:12:25 -0400

    cleaned up App.js import statements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { Route, Switch, Router } from "react-router-dom";
 import "./App.css";
-import Home from "./Home.js";
-import History from "./History.js";
-//import Login from "./Login.js";
 import Layout from "./Layout.js";
 
 class App extends React.Component {
