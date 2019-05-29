7cbda43979af23190cd01383553ecc292ee4ff84
emb2ug <47042768+emb2ug@users.noreply.github.com>
2019-05-29 15:49:16 -0400

    fixed some formatting

---
+++ srcOLD/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HomeLayout from "./HomeLayout";
+
+function App() {
+  return (
+    <div className="NApp">
+      <HomeLayout />
+    </div>
+  );
+}
+
+export default App;
