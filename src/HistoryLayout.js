d2a850ebe51b9b4976717f2eb14f34529b699d4f
Leandra Klein <lok2ea@virginia.edu>
2019-05-30 11:00:53 -0400

    history layout changed to dropdown menu

--- src/HistoryLayout.js
+++ src/HistoryLayout.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Layout, Menu, Breadcrumb, Icon } from "antd";
 import "antd/dist/antd.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // https://reacttraining.com/react-router/web/example/sidebar
 
@@ -10,59 +9,113 @@ const { Header, Content, Sider } = Layout;
 
 let mountNode = document.getElementById("root");
 
-export default class HistoryLayout extends React.Component {
-  handleClick = e => {
-    console.log("click ", e);
-  };
+const SubMenu = Menu.SubMenu;
 
+export default class HistoryLayout extends React.Component {
+  // submenu keys of first level
+  rootSubmenuKeys = ["sub1", "sub2", "sub3"];
+  state = {
+    openKeys: ["sub1"]
+  };
+  onOpenChange = openKeys => {
+    const latestOpenKey = openKeys.find(
+      key => this.state.openKeys.indexOf(key) === -1
+    );
+    if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      this.setState({ openKeys });
+    } else {
+      this.setState({
+        openKeys: latestOpenKey ? [latestOpenKey] : []
+      });
+    }
+  };
   render() {
     return (
-      <Router>
+      <Layout>
+        <Header
+          className="header"
+          style={{ background: "#fff", height: "20px" }}
+        />
         <Layout>
-          <Header
-            className="header"
-            style={{ background: "#fff", height: "20px" }}
-          >
-            <div className="logo" />
-          </Header>
-          <Layout>
-            <Sider width={150} style={{ background: "#fff", display: "flex" }}>
-              <Menu
-                onClick={this.handleClick}
-                style={{ width: 125 }}
-                defaultSelectedKeys={[]}
-                defaultOpenKeys={["sub1"]}
-                mode="inline"
+          <Sider width={150} style={{ background: "#fff", display: "flex" }}>
+            <Menu
+              onClick={this.handleClick}
+              style={{ width: 125 }}
+              defaultSelectedKeys={[]}
+              defaultOpenKeys={["sub1"]}
+              mode="inline"
+            >
+              <Menu.Item key="home">
+                <span>
+                  <Icon type="home" />
+                  <span>Home</span>
+                </span>
+                <a href={"/home"} />
+              </Menu.Item>
+              <Menu.Item key="history">
+                <span>
+                  <Icon type="history" />
+                  <span>History</span>
+                </span>
+                <a href={"/history"} />
+              </Menu.Item>
+              <Menu.Item key="logout">
+                <span>
+                  <Icon type="logout" />
+                  <span>Logout</span>
+                </span>
+                <a href={"/"} />
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ background: "#fff" }}>
+            <Menu
+              mode="inline"
+              openKeys={this.state.openKeys}
+              onOpenChange={this.onOpenChange}
+              style={{ width: 240 }}
+            >
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <span>Date (most recent changes)</span>
+                  </span>
+                }
               >
-                <Menu.Item key="home">
+                <Menu.Item key="1">Task 3</Menu.Item>
+                <Menu.Item key="2">Task 2</Menu.Item>
+                <Menu.Item key="3">Task 1</Menu.Item>
+                <Menu.Item key="4">Etc</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub2"
+                title={
                   <span>
-                    <Icon type="home" />
-                    <span>Home</span>
+                    <span>Date 2</span>
                   </span>
-                  <a href={"/home"} />
-                </Menu.Item>
-                <Menu.Item key="history">
+                }
+              >
+                <Menu.Item key="5">Task 2</Menu.Item>
+                <Menu.Item key="6">Task 1</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub3"
+                title={
                   <span>
-                    <Icon type="history" />
-                    <span>History</span>
+                    <span>Date 3</span>
                   </span>
-                  <a href={"/history"} />
-                </Menu.Item>
-                <Menu.Item key="logout">
-                  <span>
-                    <Icon type="logout" />
-                    <span>Logout</span>
-                  </span>
-                  <a href={"/"} />
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Content style={{ background: "#fff" }}>
-              <div>History log here</div>
-            </Content>
-          </Layout>
+                }
+              >
+                <Menu.Item key="9">Task 4</Menu.Item>
+                <Menu.Item key="10">Task 3</Menu.Item>
+                <Menu.Item key="11">Task 2</Menu.Item>
+                <Menu.Item key="12">Task 1</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Content>
         </Layout>
-      </Router>
+      </Layout>
     );
   }
 }
