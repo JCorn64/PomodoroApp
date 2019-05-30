1e6b91438f033de97cc8602874b11816fa346a61
JCorn64 <juliancorn64@gmail.com>
2019-05-30 09:01:45 -0400

    Fixed timer so when it get to 1 it no longer rezises container

--- srcOLD/Login.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Login.css";
-
-export default class Login extends React.Component {
-  render() {
-    return <div />;
-  }
-}
--- srcOLD/TimerC/Helpers.js
+++
@@ -1,4 +0,0 @@
-// MINUTES to MILLISECONDS
-export const _25 = 1500000;
-export const _05 = 300000;
-export const _test = 15000;
